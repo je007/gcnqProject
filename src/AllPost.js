8382c3508eb49566ac91c537e1f590147641a02d
Quinn Headley <quinnheadley18@gmail.com>
2018-10-23 20:10:23 -0700

    REact is totes here

---
+++ src/AllPost.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import Post from './Post';
+import EditComponent from './EditComponent';
+import axios from 'axios';
+
+class AllPost extends Component {
+    componentDidMount = () => {
+        axios.get('http://localhost:5000').then(res => this.props.dispatch({type:'LOAD_POSTS', data:res.data}));
+    }
+
+    render() {
+        return (
+            <div>
+                <h1 className="post_heading">All Posts</h1>
+                {this.props.posts.map((post) => (
+                    <div key={post.id}>
+                        {post.editing ? <EditComponent post={post} key={post.id} /> : <Post post={post}
+                            key={post.id} />}
+                    </div>
+                ))}
+            </div>
+        );
+    }
+}
+
+const mapStateToProps = (state) => {
+    return {
+        posts: state
+    }
+}
+export default connect(mapStateToProps)(AllPost);
